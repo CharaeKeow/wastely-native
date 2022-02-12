66d8ea139900fa7592405f2235131a90e630749f
CharaeKeow <charaekeow11@gmail.com>
2022-02-13 00:15:06 +0800

    initial commit with default bottom nav activity

---
+++ app/src/main/java/my/edu/utem/ftmk/bitp3453/achifapp/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package my.edu.utem.ftmk.bitp3453.achifapp.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/my/edu/utem/ftmk/bitp3453/achifapp/ui/home/HomeFragment.java
@@ -0,0 +1,46 @@
+package my.edu.utem.ftmk.bitp3453.achifapp.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import my.edu.utem.ftmk.bitp3453.achifapp.R;
+import my.edu.utem.ftmk.bitp3453.achifapp.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
